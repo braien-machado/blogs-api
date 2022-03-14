2725b8c2da560146e253630995da5ee5c1cdec99
Braien Machado de Paula <braienmp@outlook.com>
2022-03-14 09:30:28 -0300

    fix: create Categories model and migration

---
+++ models/category.js
@@ -0,0 +1,9 @@
+const Category = (sequelize, DataTypes) => {
+  const category = sequelize.define('Category', {
+    name: DataTypes.STRING,
+  }, { tableName: 'Categories', timestamps: false });
+
+  return category;
+};
+
+module.exports = Category;
