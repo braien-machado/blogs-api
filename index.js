71ed222e4e3970df07d7076402eabb8407e63b44
Braien Machado de Paula <braienmp@outlook.com>
2022-03-14 11:24:13 -0300

    fix: require controllers and middlewares for api

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+const controllers = require('./controllers');
+const middlewares = require('./middlewares');
+
 const app = express();
+app.use(express.json());
+
+app.post('/user', middlewares.validateNewUser, controllers.User);
+
+app.use(middlewares.errorHandler);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
