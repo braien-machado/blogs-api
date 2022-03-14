ee0d047e55f5c828e39ea101f188977183b2808e
Braien Machado de Paula <braienmp@outlook.com>
2022-03-14 13:26:02 -0300

    fix: createUser and generateToken

--- services/User.js
+++ services/User.js
@@ -1,16 +1,34 @@
+const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
-const findByEmail = async (email) => {
-  try {
-    const user = await User.findAll({ where: { email } });
+const secret = process.env.JWT_SECRET;
 
-    if (user.length > 0) return true;
-    return false;
-  } catch (error) {
-    return error;
-  }
+const findByEmail = async (email) => {
+  const user = await User.findAll({ where: { email } });
+
+  if (user.length > 0) return true;
+  return false;
+};
+
+const createUser = async (newUserInfo) => {
+  const newUser = await User.create(newUserInfo);
+
+  return newUser;
+};
+
+const generateToken = (data) => {
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+  
+  const token = jwt.sign({ data }, secret, jwtConfig);
+  
+  return token;
 };
 
 module.exports = {
   findByEmail,
+  createUser,
+  generateToken,
 };
