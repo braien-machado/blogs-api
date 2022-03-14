2e0f9366d8a7e643ccb169e681bb822f659b8754
Braien Machado de Paula <braienmp@outlook.com>
2022-03-14 13:26:43 -0300

    fix: finish POST /user endpoint

--- controllers/User.js
+++ controllers/User.js
@@ -1,5 +1,14 @@
-module.exports = (req, res) => {
-  // const { displayName, email, password, image } = req.body;
+const { createUser, generateToken } = require('../services/User');
 
-  res.status(200).json({ message: 'testing' });
+module.exports = async (req, res, next) => {
+  try {
+    const { displayName, email, password, image } = req.body;
+
+    const createdUser = await createUser({ displayName, email, password, image });
+    const token = await generateToken(createdUser);
+
+    res.status(201).json({ token });
+  } catch (error) {
+    next(error);
+  }
 };
