288d2650181007d97ea598bb32b33bc5dc746a25
Braien Machado de Paula <braienmp@outlook.com>
2022-03-14 17:42:57 -0300

    style: errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 module.exports = (err, _req, res, _next) => {
   if (err.code) return res.status(err.code).json({ message: err.message });
-
-  res.status(500).json({ message: err.message });
+  if (err.message) return res.status(500).json({ message: err.message });
+  res.status(500).json({ message: 'Something went wrong' });
 };
