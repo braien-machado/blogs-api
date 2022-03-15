605cd2d7248c6a3e67a633eb9843fc0d48128e96
Braien Machado de Paula <braienmp@outlook.com>
2022-03-15 14:53:51 -0300

    fix: export new middleware from index

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,8 +2,10 @@ const validateUser = require('./validateUser');
 const errorHandler = require('./errorHandler');
 const validatePost = require('./validatePost');
 const validatePostUpdate = require('./validatePostUpdate');
+const validateAuthorizedUser = require('./validateAuthorizedUser');
 
 module.exports = {
+  validateAuthorizedUser,
   validatePostUpdate,
   validateUser,
   validatePost,
