aec2a3ec86e60e762ebe5bcff095767530383587
Braien Machado de Paula <braienmp@outlook.com>
2022-03-15 14:53:16 -0300

    fix: create middleware to check authorized user

---
+++ middlewares/validateAuthorizedUser.js
@@ -0,0 +1,17 @@
+const Post = require('../services/Post');
+
+module.exports = async (req, res, next) => {
+  try {
+    const { params: { id: postId }, userId } = req;
+    const post = await Post.getPostByParam('id', postId);
+
+    if (!post) return next({ code: 404, message: 'Post does not exist' });
+    const { userId: creatorId } = post;
+
+    if (userId !== creatorId) return next({ code: 401, message: 'Unauthorized user' });
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
--- middlewares/validatePostUpdate.js
+++ middlewares/validatePostUpdate.js
@@ -1,11 +1,7 @@
-const Post = require('../services/Post');
-
 const searchForError = (reqParams) => {
-  const { title, content, categoryIds, userId, creatorId } = reqParams;
+  const { title, content, categoryIds } = reqParams;
 
   switch (true) {
-    case (userId !== creatorId):
-      return { code: 401, message: 'Unauthorized user' };
     case (!title):
       return { code: 400, message: '"title" is required' };
     case (!content):
@@ -20,10 +16,8 @@ const searchForError = (reqParams) => {
 module.exports = async (req, _res, next) => {
   try {
     const { title, content, categoryIds } = req.body;
-    const { params: { id: postId }, userId } = req;
-    const { userId: creatorId } = await Post.getPostByParam('id', postId);
 
-    const error = searchForError({ title, content, categoryIds, userId, creatorId });
+    const error = searchForError({ title, content, categoryIds });
 
     if (error) return next(error);
 
