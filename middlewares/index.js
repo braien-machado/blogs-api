f2026f96ae9033ec7fb239c64ce5da9faddf2d05
Braien Machado de Paula <braienmp@outlook.com>
2022-03-15 11:54:06 -0300

    fix: add validatePostUpdate to index

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,8 +1,10 @@
 const validateUser = require('./validateUser');
 const errorHandler = require('./errorHandler');
 const validatePost = require('./validatePost');
+const validatePostUpdate = require('./validatePostUpdate');
 
 module.exports = {
+  validatePostUpdate,
   validateUser,
   validatePost,
   errorHandler,
